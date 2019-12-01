25aefb6661a801c895f3abd17735d7f0c82c8681
AlekseiChirkov <tektonikboy98@gmail.com>
2019-12-02 05:45:54 +0600

    ok

--- sparta/settings.py
+++ sparta/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -101,7 +101,7 @@ else:
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'sparta',
             'USER': 'postgres',
-            'PASSWORD': os.getenv('DB_PASS', 'Optional default value'),
+            'PASSWORD': os.environ['DB_PASS'],
             'HOST': '127.0.0.1',
             'PORT': '5432',
         }
