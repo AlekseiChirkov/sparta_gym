b2bad8b1de206a20c0dc65983c9b6c2f839ae04a
AlekseiChirkov <tektonikboy98@gmail.com>
2019-12-04 17:30:37 +0600

    ok

--- reviews/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from ..api.models import *
-
-
-class Reviews(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    user_role = models.ForeignKey(Role, on_delete=models.CASCADE)
-    review = models.TextField(max_length=256)
