afcfe348d32cc716e0c4231efde6e997a9591282
AlekseiChirkov <tektonikboy98@gmail.com>
2020-01-07 09:52:35 +0600

    get ready to deploy

--- api/views.py
+++ api/views.py
@@ -1,7 +1,15 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.views import generic
+from django.contrib.auth import login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 from rest_framework.exceptions import ParseError
