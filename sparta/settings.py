a14b36ea62dfb1c592ba9317998c34b510828ade
AlekseiChirkov <tektonikboy98@gmail.com>
2020-01-10 09:37:57 +0600

    ok

--- sparta/settings.py
+++ sparta/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'cyjxoy#%df#@02xd3$75&-p=f6!q&gqstx#+8t)eis0ey@+jxg' #config('SECRE
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False #config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['159.65.217.210']
+ALLOWED_HOSTS = ['208.68.36.62', 'sparta-gym.com.kg']
 
 
 # Application definition
